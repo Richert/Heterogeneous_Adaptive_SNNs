71220a4222653866d7aa2bc243bae46a0d8156af
Richert <richgast.90@gmail.com>
2025-10-02 15:37:34 -0700

    added scripts for analytic weight distribution calculation

--- qif_simulations/qif_weight_simulation.py
+++ qif_simulations/qif_weight_simulation.py
@@ -133,43 +133,43 @@ conn = f"{conn}_inh" if conn < 0 else f"{conn}"
 f = open(f"/home/richard-gast/PycharmProjects/Heterogeneous_Adaptive_SNNs/results/qif_weight_simulation_{condition}_{conn}_{rep}.pkl", "wb")
 pickle.dump(res, f)
 
-# plotting
-fig, axes = plt.subplots(nrows=3, ncols=len(bs), figsize=(3*len(bs), 5), layout="constrained")
-ticks = np.arange(0, m, int(m/5))
-for i, b in enumerate(bs):
-
-    # weight distribution
-    ax = axes[0, i]
-    im = ax.imshow(np.asarray(res["w"][b]).T, aspect="auto", interpolation="none", cmap="viridis", vmax=1.0, vmin=0.0)
-    ax.set_ylabel("neuron")
-    ax.set_xlabel("Delta")
-    ax.set_xticks(ticks, labels=np.round(deltas[ticks], decimals=1))
-    if i == len(bs) - 1:
-        plt.colorbar(im, ax=ax, shrink=0.8)
-    ax.set_title(f"w (b = {b})")
-
-    # correlation
-    ax = axes[1, i]
-    ax.plot(deltas, res["C"][b])
-    ax.set_xlabel("Delta")
-    ax.set_ylabel("C")
-    ax.set_title("correlation(w, eta)")
-
-    # entropy
-    ax = axes[2, i]
-    ax.plot(deltas, res["H"][b])
-    ax.set_xlabel("Delta")
-    ax.set_ylabel("H")
-    ax.set_title("entropy(w)")
-
-    # # variance
-    # ax = axes[3, i]
-    # ax.plot(deltas, res["V"][b])
-    # ax.set_xlabel("Delta")
-    # ax.set_ylabel("var")
-    # ax.set_title("variance(w)")
-
-fig.suptitle(f"{'Anti-Hebbian' if 'antihebbian' in condition else 'Hebbian'} Learning (J = {int(J)}, QIF Simulation)")
-fig.canvas.draw()
-plt.savefig(f"../results/qif_weight_simulation_{condition}_{int(J)}.svg")
-plt.show()
+# # plotting
+# fig, axes = plt.subplots(nrows=3, ncols=len(bs), figsize=(3*len(bs), 5), layout="constrained")
+# ticks = np.arange(0, m, int(m/5))
+# for i, b in enumerate(bs):
+#
+#     # weight distribution
+#     ax = axes[0, i]
+#     im = ax.imshow(np.asarray(res["w"][b]).T, aspect="auto", interpolation="none", cmap="viridis", vmax=1.0, vmin=0.0)
+#     ax.set_ylabel("neuron")
+#     ax.set_xlabel("Delta")
+#     ax.set_xticks(ticks, labels=np.round(deltas[ticks], decimals=1))
+#     if i == len(bs) - 1:
+#         plt.colorbar(im, ax=ax, shrink=0.8)
+#     ax.set_title(f"w (b = {b})")
+#
+#     # correlation
+#     ax = axes[1, i]
+#     ax.plot(deltas, res["C"][b])
+#     ax.set_xlabel("Delta")
+#     ax.set_ylabel("C")
+#     ax.set_title("correlation(w, eta)")
+#
+#     # entropy
+#     ax = axes[2, i]
+#     ax.plot(deltas, res["H"][b])
+#     ax.set_xlabel("Delta")
+#     ax.set_ylabel("H")
+#     ax.set_title("entropy(w)")
+#
+#     # # variance
+#     # ax = axes[3, i]
+#     # ax.plot(deltas, res["V"][b])
+#     # ax.set_xlabel("Delta")
+#     # ax.set_ylabel("var")
+#     # ax.set_title("variance(w)")
+#
+# fig.suptitle(f"{'Anti-Hebbian' if 'antihebbian' in condition else 'Hebbian'} Learning (J = {int(J)}, QIF Simulation)")
+# fig.canvas.draw()
+# plt.savefig(f"../results/qif_weight_simulation_{condition}_{int(J)}.svg")
+# plt.show()
