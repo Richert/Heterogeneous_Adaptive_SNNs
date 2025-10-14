0152aa2d72b882fffdb3cbbf76322873805a7809
Richert <richgast.90@gmail.com>
2025-10-14 11:50:31 -0700

    added scripts for analytic weight distribution calculation

--- rnn/fre_mp_simulation.py
+++ rnn/fre_mp_simulation.py
@@ -94,6 +94,6 @@ clear(net)
 # ax.plot(np.mean(res["s"], axis=1), color="black")
 # plt.show()
 pickle.dump(
-    {"W": W, "eta": etas, "b": b, "Delta": Delta, "noise": noise_lvl},
+    {"W": W, "eta": etas[idx], "b": b, "Delta": Delta, "noise": noise_lvl},
     open(f"{path}/results/rnn_results/fre_mp_{int(b*10)}_{int(noise_lvl)}_{int(Delta*10.0)}_{rep}.pkl", "wb")
 )
