805a04ea8fda5352e5ec986d95a681b84206c84a
Richert <richgast.90@gmail.com>
2026-01-25 13:27:22 -0800

    new stuff

--- grid_search/qif_mpmf_stdp_grid_search.sh
+++ grid_search/qif_mpmf_stdp_grid_search.sh
@@ -2,14 +2,10 @@
 
 # set condition
 n=10
-batch_size=60
+batch_size=40
 range_end=$((n-1))
 synapses=( "exc" "inh" )
 stps=( "sd" "sf" )
-tau_ps=( 20.0 40.0 60.0 80.0 100.0 )
-tau_ds=( 20.0 40.0 60.0 80.0 100.0 )
-a_ps = ( 0.001 0.003 0.005 0.007 0.009 )
-a_ds = ( 0.001 0.003 0.005 0.007 0.009 )
 
 # limit amount of threads that each Python process can work with
 n_threads=2
@@ -21,24 +17,21 @@ export VECLIB_MAXIMUM_THREADS=$n_threads
 
 # execute python scripts in batches of batch_size
 for IDX in $(seq 0 $range_end); do
-  for tau_p in "${tau_ps[@]}"; do
-    for tau_d in "${tau_ds[@]}"; do
-      for a_p in "${a_ps[@]}"; do
-        for a_d in "${a_ds[@]}"; do
+  for syn in "${synapses[@]}"; do
+    for stp in "${stps[@]}"; do
 
-          # python calls
-          (
-          echo "Starting job #$((IDX+1)) of ${n} jobs for for p = ${p} and J = ${c}."
-          python /home/richard/PycharmProjects/Heterogeneous_Adaptive_SNNs/grid_search/qif_mpmf_stdp_simulation.py $p $tau $c $IDX
-          sleep 1
-          ) &
+        # python calls
+        (
+        echo "Starting job #$((IDX+1)) of ${n} jobs for for p = ${p} and J = ${c}."
+        python /home/richard/PycharmProjects/Heterogeneous_Adaptive_SNNs/grid_search/qif_mpmf_stdp_simulation.py $stp $syn $c $IDX
+        sleep 1
+        ) &
 
         # batch control
         if [[ $(jobs -r -p | wc -l) -ge $batch_size ]]; then
               wait -n
         fi
 
-      done
     done
   done
 done
