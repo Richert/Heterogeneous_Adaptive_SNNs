fafc722e905600620e5e1ef78b05cf25093e13ea
Richert <richgast.90@gmail.com>
2026-06-12 12:10:45 -0700

    new stuff

--- grid_search/kmo_adaptive_single_sweep.py
+++ grid_search/kmo_adaptive_single_sweep.py
@@ -60,7 +60,7 @@ CONFIG = dict(
     # storage
     save_res=128,                  # block-average the final A matrix / ω axis to this size
     seed=1,
-    out_csv="/home/rgast/data/mpmf_simulations/kmo_adaptive_single_sweep.csv",
+    out_csv="/home/rgast/data/qif_plasticity/kmo_adaptive_single_sweep.csv",
 )
 
 
